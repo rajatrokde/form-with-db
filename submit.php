989eb5d1428d33af3b54a956d90cf76ba2583604
root <root@ip-172-31-24-183.ap-southeast-1.compute.internal>
2024-08-17 10:48:41 +0000

    added

---
+++ submit.php
@@ -0,0 +1,27 @@
+<?php
+$servername = "localhost";
+$username = "form_user";
+$password = "password";
+$dbname = "simple_form";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+
+$sql = "INSERT INTO submissions (name, email) VALUES ('$name', '$email')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
